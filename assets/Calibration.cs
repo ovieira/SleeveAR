dbfece6f4d526ebb4a177e8fa0292f425951f901
João Vieira <felixvieira@gmail.com>
2015-06-19 12:51:10 +0100

    new calibration values

--- assets/Calibration.cs
+++ assets/Calibration.cs
@@ -8,11 +8,13 @@ public class Calibration : MonoBehaviour {
     //Projection physical size
     public float _projectedArea_X = 425f; //cm
     public float _projectedArea_Z = 328.5f; //cm
-   
+    public Vector3 offset;
 
+    [Range(0.5f, 1.5f)]
+    public float factorX, factorY;
 
     //
-    public GameObject _calibrationSprite;
+    public GameObject[] _calibrationSprite;
     #endregion
 
 
@@ -22,13 +24,21 @@ public class Calibration : MonoBehaviour {
     {
         _mt = ManagerTracking.instance;
         _dw = UIDebugWindow.instance;
-        _dw.debuggedObject = _calibrationSprite.gameObject;
+        //_dw.debuggedObject = _calibrationSprite.gameObject;
 
     }
 
     public void Update()
     {
-        _calibrationSprite.transform.position = _mt.getRigidBodyTransform(0).position;
+        //Vector3 position = _mt.getRigidBodyTransform(0).position;
+        //Vector3 positionTimesFactor = new Vector3(position.x * factorX, position.y, position.z * factorY);
+        //_calibrationSprite.transform.position = positionTimesFactor + offset;
+
+        for (int i = 0; i < _calibrationSprite.Length; i++) {
+            Vector3 position = _mt.getRigidBodyTransform(i).position;
+            Vector3 positionTimesFactor = new Vector3(position.x * factorX, position.y, position.z * factorY);
+            _calibrationSprite[i].transform.position = positionTimesFactor + offset;
+        }
     }
 
     #endregion
