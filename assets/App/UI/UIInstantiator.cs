8b0bbb88050560dc488bd777e8cd6e54521d2a00
João Vieira <felixvieira@gmail.com>
2015-07-17 00:53:02 +0100

    Specific sections are instantiated at runtime, uilearning done, next teaching ui

---
+++ assets/App/UI/UIInstantiator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIInstantiator : MonoBehaviour {
+
+
+    #region Prefab
+
+    public GameObject[] prefabs;
+
+    #endregion
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	    if (prefabs == null)
+	    {
+	        Debug.LogError("prefab is null");
+	        return;
+	    }
+
+	    foreach (var prefab in prefabs)
+	    {
+            GameObject ob = Instantiate(prefab);
+            ob.transform.SetParent(this.transform, false);
+	    }
+
+	}
+}
---
+++ assets/App/scripts/ServiceSection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography;
+
+public class ServiceSection {
+
+    #region Section
+    public enum Section {
+        LEARNING,
+        TEACHING
+    }
+
+
+    public EventHandler<EventArgs> onSectionChanged; 
+    protected Section _selected;
+
+    public Section selected
+    {
+        get { return this._selected; }
+        set
+        {
+            if (this._selected != value)
+            {
+                this._selected = value;
+                Utils.LaunchEvent(this, onSectionChanged);
+            }
+
+        }
+    }
+
+    #endregion
+
+    #region Singleton
+
+    private static ServiceSection _instance;
+
+    public static ServiceSection instance {
+        get {
+            if (_instance == null) {
+                _instance = new ServiceSection();
+            }
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    //public void LoadLearningPhase()
+    //{
+    //    Application.LoadLevel("Learning");
+    //}
+
+    //public void LoadAudioPhase()
+    //{
+    //    Application.LoadLevel("Sound");        
+    //}
+
+    //public void LoadVisualPhase() {
+    //    Application.LoadLevel("Visual");
+    //}
+}
