055f21c54a66c95f12470550adad09f853306cee
João Vieira <felixvieira@gmail.com>
2015-07-19 18:12:38 +0100

    created teaching service and controller

---
+++ assets/App/Stage/Teaching/ControllerTeaching.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerTeaching : MonoBehaviour {
+
+    #region LifeCycle
+    // Use this for initialization
+    void Start() {
+        ServiceTeaching.instance.reachedInitialPosition = false;
+
+        ServiceTeaching.instance.onReachedInitialPosition += this._onReachedInitialPosition; 
+
+        ServiceExercise.instance.onSelectedExerciseChanged += this._onSelectedExerciseChanged;
+    }
+
+
+    // Update is called once per frame
+
+    void Update() {
+
+    }
+
+    #endregion
+
+    #region Service Exercise
+
+    private void _onSelectedExerciseChanged(object sender, System.EventArgs e)
+    {
+        ServiceTeaching.instance.reachedInitialPosition = false;
+    }
+
+    #endregion
+
+    #region Service Teaching
+
+    private void _onReachedInitialPosition(object sender, System.EventArgs e) {
+        Debug.Log("Start guiding");
+    }
+
+    #endregion
+}
---
+++ assets/App/Stage/Teaching/ServiceTeaching.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class ServiceTeaching {
+
+    #region Initial Position
+
+    public EventHandler<EventArgs> onReachedInitialPosition;
+
+    protected bool _reachedInitialPosition = false;
+
+    public bool reachedInitialPosition {
+        get { return this._reachedInitialPosition; }
+        set {
+            if (this._reachedInitialPosition == value) return;
+            this._reachedInitialPosition = value;
+            if (value)
+                Utils.LaunchEvent(this, onReachedInitialPosition);
+        }
+    }
+
+    #endregion
+
+    #region Singleton
+
+    private static ServiceTeaching _instance;
+
+
+    public static ServiceTeaching instance {
+        get {
+            if (_instance == null) {
+                _instance = new ServiceTeaching();
+            }
+            return _instance;
+        }
+    }
+
+    #endregion
+}
