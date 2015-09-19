23f848aadf8588a71b0475454be5cefe53ab4526
João Vieira <felixvieira@gmail.com>
2015-09-19 20:47:36 +0100

    csv files

--- assets/App/Utils/CSVExporter.cs
+++ assets/App/Utils/CSVExporter.cs
@@ -8,16 +8,16 @@ public class CSVExporter : MonoBehaviour
     public int maxID;
 
     #region Filenames
-    public string[] Sessionfilenames;
-    public string[] VideoFilenames;
+    //public string[] Sessionfilenames;
+    //public string[] VideoFilenames;
     public string[] ExerciseFilenames; 
     #endregion
 
     #region Objects List
 
     protected List<ExerciseModel> exercises = new List<ExerciseModel>();
-    protected List<Session> sessions = new List<Session>();
-    protected List<ExerciseModel> videos = new List<ExerciseModel>();
+    //protected List<Session> sessions = new List<Session>();
+    //protected List<ExerciseModel> videos = new List<ExerciseModel>();
     
 
     #endregion
@@ -33,9 +33,6 @@ public class CSVExporter : MonoBehaviour
          Debug.Log("Loaded Original Exercises " + videos.Count);*/
 	}
 	
-	void Update () {
-	
-	}
 	#endregion
 
     #region Loading
@@ -64,62 +61,50 @@ public class CSVExporter : MonoBehaviour
     #endregion
 
 
-    #region Visualize
+    //#region Visualize
 
-    public LineRenderer original;
-    public LineRenderer session;
-    public LineRenderer video;
+    //public LineRenderer original;
+    //public LineRenderer session;
+    //public LineRenderer video;
 
-    [ContextMenu("draworiginal")]
-    public void drawOriginal()
-    {
-        var em = exercises[0];
-        original.SetVertexCount(em.exerciseModel.Count);
-        for (int i = 0; i < em.exerciseModel.Count; i++)
-        {
-            var jg = em.exerciseModel[i];
-            var upperDir = jg.getUpperArmDirection();
+    //[ContextMenu("draworiginal")]
+    //public void drawOriginal()
+    //{
+    //    var em = exercises[0];
+    //    original.SetVertexCount(em.exerciseModel.Count);
+    //    for (int i = 0; i < em.exerciseModel.Count; i++)
+    //    {
+    //        var jg = em.exerciseModel[i];
+    //        var upperDir = jg.getUpperArmDirection();
 
-            original.SetPosition(i, upperDir*5);
-        }
-    }
+    //        original.SetPosition(i, upperDir*5);
+    //    }
+    //}
 
-    [ContextMenu("drawsession")]
-    public void drawSession() {
-        var sess = sessions[0].logs[2].entries;
-        session.SetVertexCount(sess.Count);
-        for (int i = 0; i < sess.Count; i++) {
-            var jg = sess[i].jointsGroup;
-            var upperDir = jg.getUpperArmDirection();
+    //[ContextMenu("drawsession")]
+    //public void drawSession() {
+    //    var sess = sessions[0].logs[2].entries;
+    //    session.SetVertexCount(sess.Count);
+    //    for (int i = 0; i < sess.Count; i++) {
+    //        var jg = sess[i].jointsGroup;
+    //        var upperDir = jg.getUpperArmDirection();
 
-            session.SetPosition(i, upperDir * 5);
-        }
-    }
+    //        session.SetPosition(i, upperDir * 5);
+    //    }
+    //}
 
-    [ContextMenu("drawvideo")]
-    public void drawVideo() {
-        var em = videos[0];
-        video.SetVertexCount(em.exerciseModel.Count);
-        for (int i = 0; i < em.exerciseModel.Count; i++) {
-            var jg = em.exerciseModel[i];
-            var upperDir = jg.getUpperArmDirection();
+    //[ContextMenu("drawvideo")]
+    //public void drawVideo() {
+    //    var em = videos[0];
+    //    video.SetVertexCount(em.exerciseModel.Count);
+    //    for (int i = 0; i < em.exerciseModel.Count; i++) {
+    //        var jg = em.exerciseModel[i];
+    //        var upperDir = jg.getUpperArmDirection();
 
-            video.SetPosition(i, upperDir * 5);
-        }
-    }
-    #endregion
-
-    [ContextMenu("sumdistOriginalSession")]
-    public void sumdistorisess()
-    {
-        var sessionlst = sessions[0].logs[2].LogToJointsGroupsList();
-        SumOfMinimumDistances(exercises[0].exerciseModel,sessionlst);
-    }
-
-    [ContextMenu("sumdistOriginalVideo")]
-    public void sumdistorivideo() {
-        SumOfMinimumDistances(exercises[0].exerciseModel, videos[0].exerciseModel);
-    }
+    //        video.SetPosition(i, upperDir * 5);
+    //    }
+    //}
+    //#endregion
 
     #region Sum of Minimum Distances
     public float SumOfMinimumDistances(List<JointsGroup> original, List<JointsGroup> copy) {
@@ -140,37 +125,33 @@ public class CSVExporter : MonoBehaviour
     #endregion
 
 
-    [ContextMenu("test")]
-    public void generateSumofMinDistSession()
-    {
+    #region Session Table
+    [ContextMenu("Session Table")]
+    public void generateSumofMinDistSession() {
         string csvString =
             "id;ex1_try1;ex1_try2;ex1_try3;ex2_try1;ex2_try2;ex2_try3;ex3_try1;ex3_try2;ex3_try3;ex4_try1;ex4_try2;ex4_try3;ex5_try1;ex5_try2;ex5_try3\n";
 
-        for (int i = 1; i <= maxID; i++)
-        {
+        for (int i = 1; i <= maxID; i++) {
             print("Writing session " + i);
-            string entry = sumofmindistentry(i);
+            string entry = LogEntrySOMDSession(i);
             csvString += (entry + "\n");
 
         }
 
-        ServiceFileManager.instance.WriteToFile("test.csv",csvString);
+        ServiceFileManager.instance.WriteToFile("Sessions.csv", csvString);
     }
 
-    private string sumofmindistentry(int i)
-    {
+    private string LogEntrySOMDSession(int i) {
         string s = "";
         List<Session> listsessions = new List<Session>();
         getSessionsFromID(listsessions, i);
-        s += (i+";");
+        s += (i + ";");
 
-        foreach (var listsession in listsessions)
-        {
+        foreach (var listsession in listsessions) {
             int exID = Int32.Parse(listsession.exerciseID);
-            foreach (var log in listsession.logs)
-            {
+            foreach (var log in listsession.logs) {
                 var result = SumOfMinimumDistances(exercises[exID - 1].exerciseModel, log.LogToJointsGroupsList());
-                s += (result+";");
+                s += (result + ";");
             }
         }
 
@@ -179,15 +160,89 @@ public class CSVExporter : MonoBehaviour
 
     private void getSessionsFromID(List<Session> listsessions, int i) {
 
-        for (int j = 1; j <= 5; j++)
-        {
+        for (int j = 1; j <= 5; j++) {
             var s = ServiceFileManager.instance.LoadSession("" + i + "_" + j);
             listsessions.Add(s);
         }
 
+    } 
+    #endregion
+
+    #region AVG Session Table
+
+    [ContextMenu("Average Session Table")]
+    public void AVGgenerateSumofMinDistSession() {
+        string csvString =
+            "id;ex1;ex2;ex3;ex4;ex5\n";
+        for (int i = 1; i <= maxID; i++) {
+            string entry = AVGLogEntrySOMDSession(i);
+            csvString += (entry + "\n");
+
+        }
+
+        ServiceFileManager.instance.WriteToFile("AVGSessions.csv", csvString);
     }
 
-    
+    private string AVGLogEntrySOMDSession(int i) {
+        string s = "";
+        List<Session> listsessions = new List<Session>();
+        getSessionsFromID(listsessions, i);
+        s += (i + ";");
 
-    
+        foreach (var listsession in listsessions) {
+            int exID = Int32.Parse(listsession.exerciseID);
+            float f = 0;
+            foreach (var log in listsession.logs) {
+                var result = SumOfMinimumDistances(exercises[exID - 1].exerciseModel, log.LogToJointsGroupsList());
+                f += result;
+                //s += (result + ";");
+            }
+            f = f/3f;
+            s += (f + ";");
+        }
+
+        return s;
+    }
+
+    #endregion
+
+
+    #region Video Table
+    [ContextMenu("Video Table")]
+
+    public void generateSumOfMinDistVideo() {
+        string csvString = "id;ex1;ex2;ex3;ex4;ex5\n";
+
+        for (int i = 1; i <= maxID; i++) {
+            string entry = VideoSession(i);
+            csvString += (entry + "\n");
+        }
+
+        ServiceFileManager.instance.WriteToFile("Videos.csv", csvString);
+    }
+
+    private string VideoSession(int i) {
+        string s = "";
+        List<ExerciseModel> videoslist = new List<ExerciseModel>();
+        getVideosFromID(videoslist, i);
+
+        s += (i + ";");
+
+        for (int j = 0; j < videoslist.Count; j++) {
+            var sum = SumOfMinimumDistances(exercises[j].exerciseModel, videoslist[j].exerciseModel);
+            s += (sum + ";");
+        }
+
+        return s;
+    }
+
+    private void getVideosFromID(List<ExerciseModel> videoslist, int i) {
+        for (int j = 1; j <= 5; j++) {
+            var v = ServiceFileManager.instance.LoadExerciseModel("video" + i + "_" + j);
+            videoslist.Add(v);
+        }
+    } 
+    #endregion
+
+  
 }
